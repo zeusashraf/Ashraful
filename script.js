2b6b6016f04b3e4f12dc0a462e09f651b855eddc
zeusashraf <47635073+zeusashraf@users.noreply.github.com>
2019-02-14 06:12:18 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    
+          var yourname = prompt("What is your name");
+if (yourname != null){
+                   document.getElementById("sayhello").innerHTML="hi"+yourname;}
+else
+    {
+        alert("Alert!");
+    }
+               
